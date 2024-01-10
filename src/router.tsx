73c16556fcde8f7521476581dac0690eff580e6b
Bezouro <bruno@bezouro.com.br>
2024-01-10 04:32:40 -0300

    fix: set home as single page

--- src/router.tsx
+++ src/router.tsx
@@ -1,20 +1,11 @@
 import {createBrowserRouter} from "react-router-dom";
 import Home from "./pages/home";
-import Index from "./pages/app";
 
 const routes = createBrowserRouter([
   {
     path: '/',
-    element: <Index />,
-  },
-  {
-    path: '/home',
     element: <Home />,
   },
-  {
-    path: '/login',
-    element: <div>login</div>,
-  }
 ]);
 
 export default routes;
